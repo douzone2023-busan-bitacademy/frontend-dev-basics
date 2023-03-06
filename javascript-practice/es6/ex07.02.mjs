5c0217052831bc610993a98eb9960b376e30cc82
douzone2023-busan-bitacademy <douzone2023.busan.bitacademy@gmail.com>
2023-03-06 14:49:08 +0900

    es6 #3

---
+++ javascript-practice/es6/ex07.02.mjs
@@ -0,0 +1,16 @@
+
+/**
+ * 
+ * Unnamed Export: default 키워드를 사용한다.
+ * 
+ */
+export default {
+    add: function(a, b) {
+        return a + b;
+    },
+    substract: function(a, b) {
+        return a - b;
+    }
+}
+
+
